3dc33ee3ddc32e1c418969bf724d369ca31078d8
kplakhova <kplakhova@luxoft.com>
2018-11-08 17:17:42 +0100

    lambdas

---
+++ src/com/learnJava/lambdas/RunnableLambdaExample.java
@@ -0,0 +1,45 @@
+package com.learnJava.lambdas;
+
+public class RunnableLambdaExample {
+
+    public static void main(String[] args) {
+
+        /**
+         * prior Java 8
+         */
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside Runnable 1");
+            }
+        };
+        new Thread(runnable).start();
+
+        /**
+         * Java 8 lambda
+         *
+         * () -> {};
+         */
+
+        Runnable runnableLambda = () -> {
+            System.out.println("Inside Runnable 2");
+        };
+        new Thread(runnableLambda).start();
+
+        Runnable runnableLambda1 = () -> {
+            System.out.println("Inside Runnable 3");
+            System.out.println("Inside Runnable 3");
+        };
+        new Thread(runnableLambda1).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside Runnable 3.1");
+            }
+        }).start();
+
+        new Thread(()-> System.out.println("Inside Runnable 4")).start();
+    }
+}
