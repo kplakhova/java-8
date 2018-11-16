ce930b84ff421a381931d65dc287eb2debb04667
kplakhova <kplakhova@luxoft.com>
2018-11-16 17:45:37 +0100

    stream operations example

--- src/com/learnJava/streams/StreamsFlatMapExample.java
+++ src/com/learnJava/streams/StreamsFlatMapExample.java
@@ -13,14 +13,27 @@ public class StreamsFlatMapExample {
         List<String> studentActivities = StudentDataBase.getAllStudents().stream() // Stream<Student>
                 .map(Student::getActivities)      // Stream<List<String>>
                 .flatMap(List::stream)            // Stream<String>
+                .distinct()
+                .sorted()
                 .collect(Collectors.toList());
 
         return studentActivities;
     }
 
+    public static Long getStudentActivitiesCount() {
+
+        return StudentDataBase.getAllStudents().stream() // Stream<Student>
+                .map(Student::getActivities)      // Stream<List<String>>
+                .flatMap(List::stream)            // Stream<String>
+                .distinct()                       // Stream<String> -> with distinct function performed
+                .count();
+
+    }
+
     public static void main(String[] args) {
 
         System.out.println("printStudentActivities : " + printStudentActivities());
+        System.out.println("getStudentActivitiesCount : " + getStudentActivitiesCount());
 
     }
 }
